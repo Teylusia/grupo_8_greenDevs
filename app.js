8c90198f41887b6c6f900f12c0bc89b4dd6a7e1f
Teylusia <ataylor@udesa.edu.ar>
2022-02-02 10:12:54 -0300

    estructura basica de carpetas, app.js, node modules, express y nodemon.

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const publicPath = path.resolve(__dirname,'public')
+app.use( express.static(publicPath))
+
+app.listen(3000, () =>{
+  console.log('Arrancando servidor...')
+})
+app.get('/', (req, res)=>{
+  res.sendFile(__dirname + '/views/home.html')
+})
