52e2fda5fb94da0221d066a01eb494d2482e5d72
leoneld80 <leoneld80@gmail.com>
2022-02-04 19:38:52 -0300

    agregue rutas a las paginas html

--- app.js
+++ app.js
@@ -7,7 +7,26 @@ app.use( express.static(publicPath))
 
 app.listen(3000, () =>{
   console.log('Arrancando servidor...')
+  console.log("http://localhost:3000")
 })
+
+//ROUTES
 app.get('/', (req, res)=>{
   res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/login', (req, res)=>{
+  res.sendFile(__dirname + '/views/login.html')
+})
+
+app.get('/productcart', (req, res)=>{
+  res.sendFile(__dirname + '/views/productCart.html')
+})
+
+app.get('/productdetail', (req, res)=>{
+  res.sendFile(__dirname + '/views/productDetail.html')
+})
+
+app.get('/register', (req, res)=>{
+  res.sendFile(__dirname + '/views/register.html')
 })
