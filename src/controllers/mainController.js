401813a4a5ac6c45ebc33da68061d49ff7b345ef
leoneld80 <leoneld80@gmail.com>
2022-03-23 20:23:59 -0300

    los productos en el index se cargan desde el JSON

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,8 +1,12 @@
-
+const path = require('path');
+const fs = require('fs')
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 let controller = {
   home:  (req, res)=>{
-    res.render('index')
+    res.render('index', {products:products})
+    
   },
 
   login: (req, res)=>{
