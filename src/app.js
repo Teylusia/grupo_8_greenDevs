67bbb6d9ed246c942d4ef24d864e32f2baeb1476
leoneld80 <leoneld80@gmail.com>
2022-03-29 19:28:13 -0300

    arreglos en productDetail, agregue morgan tambien

--- src/app.js
+++ src/app.js
@@ -1,28 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const mainRoutes = require('./routes/mainRoutes');
-const usersRoutes = require('./routes/usersRoutes');
-const productsRoutes = require('./routes/productsRoutes')
-const publicPath = path.resolve(__dirname,'../public');
-const methodOverride = require('method-override')
+const path = require("path");
+const mainRoutes = require("./routes/mainRoutes");
+const usersRoutes = require("./routes/usersRoutes");
+const productsRoutes = require("./routes/productsRoutes");
+const publicPath = path.resolve(__dirname, "../public");
+const methodOverride = require("method-override");
+const logger = require("morgan");
 
 app.use(express.static(publicPath));
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
-app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+app.use(logger("dev"))
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
 app.use(mainRoutes);
 app.use(usersRoutes);
 app.use(productsRoutes);
 
-
-app.listen(3000, () =>{
-  console.log('Arrancando servidor...');
+app.listen(3000, () => {
+  console.log("Arrancando servidor...");
   console.log("http://localhost:3000");
 });
-
-
