c916d3e65fdd04fad334a0747b896430ad7bc34c
leoneld80 <leoneld80@gmail.com>
2022-04-14 18:52:58 -0300

    CRUD hecho falta validaciones

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,25 +1,36 @@
 const express = require('express');
 const router = express.Router(); 
 const productsController = require('../controllers/productsController');
-const multer = require('multer');
-const moduleMulter = require('../modules/modules');
 const { route } = require('./productsRoutes');
-const upload = multer({ storage: moduleMulter });
+const path = require("path");
+
+//Multer
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb){
+    cb(null, 'public/img/uploads')
+  },
+  filename: function (req, file, cb){
+    cb(null, file.fieldname+"-"+Date.now()+path.extname(file.originalname))
+  }
+})
+const upload = multer({ storage: storage });
+
 
 //Cart
-router.get('/productcart', productsController.cart);
+router.get("/productcart", productsController.cart);
 
 //Detail
 //router.get('/product/:id', productsController.detail);
-router.get('/productDetail/:id', productsController.detail); //Esta ruta permite ver el product detail
+router.get("/productDetail/:id", productsController.detail); //Esta ruta permite ver el product detail
 
-router.get('/products/edit/:id', productsController.productEdit);
-// router.put('/products/edit/:id', productsController.productEdited);
- router.delete('products/delete/:id', productsController.productDelete);
-
+//Edit-Delete
+router.get("/products/edit/:id", productsController.productEdit);
+router.put("/products/edit/:id",upload.single("image"), productsController.productEdited);
+router.delete("/products/delete/:id", productsController.productDelete);
 
 //Create Products
 router.get('/products/create', productsController.productCreate);
-router.post('/products/create', upload.single("imageProduct"), productsController.productAdd);
+router.post('/products/create', upload.single("image"), productsController.productAdd);
 
 module.exports = router;
