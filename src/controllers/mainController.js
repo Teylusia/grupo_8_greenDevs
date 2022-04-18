75974e3034a08aa60ba612eecdc6971f95bf0d01
Teylusia <ataylor@udesa.edu.ar>
2022-04-18 12:11:18 -0300

    retoques al mainRouter

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs')
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const { validationResult } = require('express-validator')
 
 let controller = {
   home:  (req, res)=>{
@@ -13,6 +14,15 @@ let controller = {
     res.render('login');
   },
 
+  loggedIn: (req,res)=>{
+
+    let errors = validationResult(req)
+    if(errors.isEmpty()){
+      res.redirect('profile');
+    }else{
+      res.render('login', {errors: errors.mapped(), old: req.body})
+    }
+  }
 };
 
 module.exports = controller;
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
-
+const { body } = require('express-validator')
 
 router.get('/register', usersController.register);
 router.post('/register', usersController.userAdd);
