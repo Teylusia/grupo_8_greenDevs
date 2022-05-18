448541db1626fcb78f5a7af088a91c06194ff372
leoneld80 <leoneld80@gmail.com>
2022-05-18 15:30:03 -0300

    arreglo en admin

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": ,
     "database": "greendevs_db",
     "host": "localhost",
     "dialect": "mysql"
