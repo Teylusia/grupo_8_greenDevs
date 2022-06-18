66c3642aa3ec60bf0295859fab2a2ad6cf3a82af
JGGuillen <ing.joseguillenm@gmail.com>
2022-06-18 15:21:38 -0300

    nuevos estilos para vista 404

--- Dashboard GreenDevs/src/components/NotFound.js
+++ Dashboard GreenDevs/src/components/NotFound.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import imagenFondo from '../assets/images/404notfound.png';
 
 function NotFound(){
     return(
-        <div className="text-center">
-            <h1>404 Not Found</h1>
-            <img className="img-fluid px-3 px-sm-4 mt-3 mb-4"  src={imagenFondo} alt=" 404 "/>
-        </div>
-        
+
+        <div>
+
+
+
+<div className="area" >
+
+          <ul className="circles">
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+
+            <div className="context">
+                <h1>Pagina no encontrada</h1>
+                <a href="/"><h6 className='back-button-404'>Presiona aqui para volver</h6></a>
+            </div>
+
+          </ul>
+
+  </div >
+  </div>
     )
 }
 
